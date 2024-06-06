617b0ca092e7ed96961287bf4c1b99385fb9e92e
pelicanpubis <norah.binsalamah@hotmail.com>
2024-06-06 22:30:02 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './components/Navbar'
-import LandingPageComp from './components/LandingPageComp'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import NavBar from "./components/Navbar";
+import LandingPageComp from "./components/LandingPageComp";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <>
+      <Router>
+        <>
+          <NavBar />
+        </>
+        <Routes></Routes>
+      </Router>
 
-    <NavBar/>
-    <LandingPageComp/>
+      <LandingPageComp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import NavBar from './components/Navbar'
+// import LandingPageComp from './components/LandingPageComp'
+
+// function App() {
+
+//   return (
+//     <>
+
+//     <NavBar/>
+//     <LandingPageComp/>
+//     </>
+//   )
+// }
+
+// export default App
