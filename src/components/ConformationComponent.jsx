1e34612f0e7a25377cc4fb37f19040d9907e533c
pelicanpubis <norah.binsalamah@hotmail.com>
2024-06-09 01:41:59 +0200

    payment component fixed

--- src/components/ConformationComponent.jsx
+++ src/components/ConformationComponent.jsx
@@ -1,6 +1,41 @@
-import React from 'react';
+
+import React from "react";
+import PaymentComponent from "./PaymentComponent";
 function ConformationComponent() {
-    return ( <h1>Hello</h1>);
+  return (
+    <>
+      <div className="conformation-container">
+        <div className="conformation-box">
+          <PaymentComponent />
+          <PaymentComponent />
+      
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ConformationComponent;
+
+
+// import React from "react";
+// import PaymentComponent from "./PaymentComponent";
+
+// function ConformationComponent() {
+//   return (
+//     <>
+//       <div className="conformation-container">
+//         <div className="conformation-box">
+//           <PaymentComponent />
+//           <PaymentComponent />
+//           <PaymentComponent />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default ConformationComponent;
+
+
+
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import SignInComponent from '../components/SignInComponent';
+function SignInPage() {
+    return ( <><SignInComponent/></> );
+}
+
+export default SignInPage;
