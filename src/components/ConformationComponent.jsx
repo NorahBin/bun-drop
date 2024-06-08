bc1b427e6a75b32e82e76952f2967a2effe9c0fb
pelicanpubis <norah.binsalamah@hotmail.com>
2024-06-09 00:14:57 +0200

    sign up button

---
+++ src/components/ConformationComponent.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function ConformationComponent() {
+    return ( <h1>Hello</h1>);
+}
+
+export default ConformationComponent;
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignUpComponent from '../components/SignUpComponent';
+
+function SignUpPage() {
+    return (  <SignUpComponent/>);
+}
+
+export default SignUpPage;
