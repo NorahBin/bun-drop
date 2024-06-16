06ec1c490af503c1559eebee5e84588997d8b23c
pelicanpubis <norah.binsalamah@hotmail.com>
2024-06-17 00:53:18 +0200

    favorite design

--- src/components/FavoriteComponent.jsx
+++ src/components/FavoriteComponent.jsx
@@ -1,27 +1,56 @@
-
 import React, { useState, useEffect } from "react";
+import stripes from "../images/stripes.png";
 
 function FavoriteComponent({ userId }) {
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     const allFavorites = JSON.parse(localStorage.getItem("favorites")) || {};
-
     setFavorites(allFavorites[userId] || []);
-  }, [userId]); 
+  }, [userId]);
+
+  const handleRemove = (itemId) => {
+    const allFavorites = JSON.parse(localStorage.getItem("favorites")) || {};
+    const userFavorites = allFavorites[userId] || [];
+    const updatedFavorites = userFavorites.filter((item) => item.id !== itemId);
+
+    allFavorites[userId] = updatedFavorites;
+    localStorage.setItem("favorites", JSON.stringify(allFavorites));
+    setFavorites(updatedFavorites);
+  };
 
   return (
     <>
-      <h1>Favorites</h1>
-      <div className="menu-card-container">
+      <div className="favorite-container">
+        <h1 className="favorite-title">
+          Favorites{" "}
+          <img src={stripes} alt="Stripes" className="stripes-image" />
+        </h1>
+        <div className="favorite-title-red-line"></div>
+      </div>
+
+      <div className="favorite-card-container">
         {favorites.map((item, index) => (
-          <div key={index} className="menu-card">
-            <img src={item.image} alt={item.title} className="burger-pic" />
-            <h1 className="card-text">{item.title}</h1>
-            <div className="red-line"></div>
+          <div key={index} className="favorite-card">
+            <img
+              src={item.image}
+              alt={item.title}
+              className="favorite-burger-pic"
+            />
+            <h1 className="favorite-card-text">{item.title}</h1>
+            <div className="favorite-red-line"></div>
             <h2>{item.name}</h2>
-            <p>{item.description}</p>
-            <p>{item.price}</p>
+            <p className="favorite-description">{item.description}</p>
+            <div className="favorite-price-container">
+              <p className="favorite-price-text">Price: </p>
+              <p className="favorite-price">{item.price}</p>
+            </div>
+            <button
+              className="favorite-remove-button"
+              onClick={() => handleRemove(item.id)}
+            >
+              Remove
+            </button>
           </div>
         ))}
       </div>
@@ -30,3 +59,4 @@ function FavoriteComponent({ userId }) {
 }
 
 export default FavoriteComponent;
+
