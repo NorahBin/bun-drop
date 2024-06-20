9cbb78e1ad00f69fd61f8e1690d88ee34fb0c677
pelicanpubis <norah.binsalamah@hotmail.com>
2024-06-20 21:31:01 +0200

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import bundrop from "../images/bundrop.png";
 import { NavLink } from "react-router-dom";
@@ -14,51 +12,50 @@ function NavBar({ user, setUser }) {
   let buttonAction = user ? handleSignOut : null;
 
   return (
-    <>
-      <div className="navbar">
-        <NavLink to="/" className="navlink">
-          <img src={bundrop} alt="Logo" className="navbar-logo" />
-        </NavLink>
-        <div className="flexbox navbar-text">
-          <NavLink
-            to="/menupage"
-            className="navlink"
-            activeClassName="active-navlink"
-          >
-            <h2 className="menu-text">MENU</h2>
-          </NavLink>
-          <NavLink
-            to="/cartpage"
-            className="navlink"
-            activeClassName="active-navlink"
-          >
-            <h2 className="cart-text">CART</h2>
-          </NavLink>
-          {user && (
-            <NavLink
-              to="/favoritepage"
-              className="navlink"
-              activeClassName="active-navlink"
-            >
-              <h2 className="favorites-text">FAVORITES</h2>
-            </NavLink>
-          )}
-          <NavLink to="/" className="navlink" activeClassName="active-navlink">
-            <h2 className="home-text">HOME</h2>
-          </NavLink>
-        </div>
+    <div className="navbar">
+      <NavLink to="/" className="navlink">
+        <img src={bundrop} alt="Logo" className="navbar-logo" />
+      </NavLink>
+      <div className="navbar-links">
         <NavLink
-          to={user ? "/" : "/signinpage"}
+          to="/menupage"
           className="navlink"
           activeClassName="active-navlink"
         >
-          <button className="sign-in" onClick={buttonAction}>
-            {buttonText}
-          </button>
+          MENU
+        </NavLink>
+        <NavLink
+          to="/cartpage"
+          className="navlink"
+          activeClassName="active-navlink"
+        >
+          CART
+        </NavLink>
+        {user && (
+          <NavLink
+            to="/favoritepage"
+            className="navlink"
+            activeClassName="active-navlink"
+          >
+            FAVORITES
+          </NavLink>
+        )}
+        <NavLink to="/" className="navlink" activeClassName="active-navlink">
+          HOME
         </NavLink>
       </div>
-    </>
+      <NavLink
+        to={user ? "/" : "/signinpage"}
+        className="navlink"
+        activeClassName="active-navlink"
+      >
+        <button className="sign-in" onClick={buttonAction}>
+          {buttonText}
+        </button>
+      </NavLink>
+    </div>
   );
 }
 
 export default NavBar;
+
